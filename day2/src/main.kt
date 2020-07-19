1bc75903424acc3365a59805ca7967eb34d5805b
punnnnnnnnn <68175882+punlnwza555@users.noreply.github.com>
2020-07-19 12:27:31 +0700

    yabbadabbado

--- day2/src/main.kt
+++ day2/src/main.kt
@@ -1,3 +1,10 @@
 fun main(){
-    println("This is Day2!!")
+    var x = 1
+
+    while (x < 3) {
+        print(if (x == 1) "Yab" else "Dab")
+        print("Ba")
+        x = x + 1
+    }
+    if (x == 3) println ("Do")
 }
