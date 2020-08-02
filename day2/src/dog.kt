bdbca9d3f8185195dfbd039fc16a10dc638cb5e2
punnnnnnnnn <68175882+punlnwza555@users.noreply.github.com>
2020-08-02 12:34:32 +0700

    2

--- day2/src/dog.kt
+++ day2/src/dog.kt
@@ -1,5 +1,18 @@
-class dog(val name : String, var weight : Int, val breed: String) {
+class dog(val name : String, weight_param: Int, breed_param: String) {
+    var activity = arrayOf("walks")
+    var breed = breed_param.toUpperCase()
+    var weight = weight_param
+        set(value) {
+            if (value > 0) field = value
+        }
+    val weightInKgs: Double
+        get() = weight / 2.2
+
+
     fun bark(){
         println(if (weight < 20) "Yip!" else "Woof!")
+    }
+    init{
+        println("dog name ${name} has been created")
     }
 }
--- day2/src/main.kt
+++ day2/src/main.kt
@@ -1,8 +1,20 @@
 fun main(args: Array<String>){
-    var d = DrumKit(true, true)
-    d.playTopHat()
-    d.playSnare()
-    d.hasSnare = false
-    d.playTopHat()
-    d.playSnare()
+    var mydog = dog("Fido", 70, "Mixed")
+
+    myDog.bark()
+
+    myDog.weight = 75
+
+    println("Weight in Kgs is ${myDog.weightInKgs}")
+
+    myDog.weight = -2
+
+    println("Weight is ${myDog.weight}")
+
+    myDog.activities = arrayOf("Walks", "Fetching balls", "Frisbee")
+
+    for (item in myDog.activities) {
+
+        println("My dog enjoys $item")
+    }
 }
